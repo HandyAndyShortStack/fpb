bd67c286312f706a9c9ed8c8b0e2ec59360d42c4
HandyAndyShortStack <andynshort@gmail.com>
2013-01-10 18:00:20 -0800

    added sidebar links

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ Meteor.startup(function() {
 
 var sidebar = $('#sidebar');
 var logo = $('#logo');
+var sidebar_links = $('.sidebar-link');
 
 sidebar.css('height', document.height);
 logo.css('height', sidebar.width());
