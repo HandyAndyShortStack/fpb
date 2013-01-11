0f96f62917e79e21e77467d31251de3c0942513f
HandyAndyShortStack <andynshort@gmail.com>
2013-01-10 17:39:05 -0800

    set size of fpb logo

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 Meteor.startup(function() {
+    if (Meteor.isClient) {
 ///////////////////////////
 
+var sidebar = $('#sidebar');
+var logo = $('#logo');
 
+sidebar.css('height', document.height);
+logo.css('height', sidebar.width());
+logo.css('width', sidebar.width());
 
-///
+/////
+    }
 });
