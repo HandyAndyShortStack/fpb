ad67d1e93cf1a18c7b89ff3e391f96438ee204dc
HandyAndyShortStack <andynshort@gmail.com>
2013-01-10 23:29:41 -0800

    add lots of pretty colors

--- client/main.js
+++ client/main.js
@@ -20,16 +20,57 @@ var observer = cursor.observe({
 // handles
 var content_links = $('.sidebar-link');
 var content_pages = $('.content');
+var topbar = $('#topbar');
 var sidebar = $('#sidebar');
-var logo = $('#logo');
+var logo = $('#logo-link');
 var sidebar_links = $('.sidebar-link');
 
 // setup
 sidebar.css('height', document.height);
-logo.css('height', sidebar.width());
-logo.css('width', sidebar.width());
+logo.attr('height', sidebar.width());
+logo.attr('width', sidebar.width());
 content_pages.hide();
 
+// fancy colors for the links
+function getRandomColor() {
+    var coin = Math.random();
+    if (coin < (1 / 6)) {
+        return 'limegreen';
+    } else if (coin < (2 / 6)) {
+        return 'orchid';
+    } else if (coin < (3 / 6)) {
+        return 'red';
+    } else if (coin < (4 / 6)) {
+        return 'yellow';
+    } else if (coin < (5 / 6)) {
+        return 'deepskyblue';
+    } else {
+        return 'orange'
+    }
+}
+content_links.on('mouseover', function() {
+    var self = $('#' + this.id);
+    var color = getRandomColor();
+    self.css('color', color);
+    logo.attr('src', 'logo_' + color + '.png');
+});
+content_links.on('mouseout', function() {
+    var self = $('#' + this.id);
+    self.css('color', 'white');
+    logo.attr('src', 'logo_white.png');
+});
+logo.on('mouseover', function() {
+    var color = getRandomColor();
+    logo.attr('src', 'logo_' + color + '.png');
+    topbar.css('color', color);
+    sidebar_links.css('color', color);
+});
+logo.on('mouseout', function() {
+    var color = getRandomColor();
+    logo.attr('src', 'logo_white.png');
+    topbar.css('color', 'white');
+    sidebar_links.css('color', 'white');
+});
 
 ///
 });
