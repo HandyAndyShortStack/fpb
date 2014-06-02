e8c9a52dc5cd43feab1ddbde8f6d44f5deb48f46
handyandyshortstack <andynshort@gmail.com>
2014-06-02 12:47:41 -0700

    sorting current shows

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,6 @@ class Show < ActiveRecord::Base
   validates_presence_of :date, :venue
 
   def self.current
-    where("date > ?", Date.today - 1.week)
+    where("date > ?", Date.today - 1.week).sort_by(&:date)
   end
 end
